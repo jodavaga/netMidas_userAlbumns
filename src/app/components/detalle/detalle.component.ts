c270915bf7433749c630f52c32455e599c3b4d36
Jodavaga <jodavaga@gmail.com>
2018-04-24 12:30:42 -0500

    User loading albums, only for this

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -13,23 +13,38 @@ export class DetalleComponent implements OnInit {
   user: any;
   id: any;
 
+  albums: any = [];
+
   constructor(private _users: UsersService,
               private activatedRoute: ActivatedRoute
               ) { }
 
 
   ngOnInit() {
-
+    // obtengo respuesta del usuario al que se le dio click
     this.activatedRoute.params
             .subscribe( respuesta => {
                 this.id = respuesta;
             });
 
+    // Obtengo la data del usuario obtenido por parametros
     this._users.getUser( this.id['id'] )
                             .subscribe( data => {
                               this.user = data;
                               console.log(this.user);
                             });
+
+    this._users.getAlbums( this.id['id'] )
+          .subscribe( data => {
+            this.eraseAlbums();
+              this.albums = data;
+              console.log(data);
+          });
+  }
+
+  eraseAlbums() {
+    console.log('borrando');
+    this._users.userAlbums = [];
   }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -9,6 +9,8 @@ export class UsersService {
   users: any = [];
   usuario: any;
 
+  userAlbums: any = [];
+
   constructor(public http: HttpClient) {
 
     console.log( ' Usuarios listos para cargarse ' );
@@ -20,7 +22,6 @@ export class UsersService {
 
     return this.http.get(url)
             .map( (respuesta: any) => {
-              console.log(respuesta);
               this.users = respuesta;
               return this.users;
             });
@@ -34,9 +35,27 @@ export class UsersService {
     return this.http.get(url)
               .map( respuesta => {
                 this.usuario = respuesta;
-                console.log(respuesta);
                 return this.usuario;
               });
+   }
+
+   getAlbums( userID: number ) {
+
+    const url = `https://jsonplaceholder.typicode.com/albums`;
+
+    return this.http.get(url)
+                  .map( respuesta => {
+
+                    for ( let user of respuesta ) {
+
+                      if ( user.userId == userID ) {
+                        this.userAlbums.push(user);
+                      }
+                    }
+                    return this.userAlbums;
+
+                  });
+
    }
 
 }
